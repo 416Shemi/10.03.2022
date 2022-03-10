9aa5151846b1e36d350db487a26b10357edd856a
416Shemi <tu7ngcsvr@code.edu.az>
2022-03-10 10:19:59 +0400

    new projekt

---
+++ ConsoleApp11/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arry1 = { 2, -3, -4, -5 };
+            int[] arry2 = arry1;
+            Console.WriteLine(menfi(arry2));
+            Console.ReadLine();
+        }
+        static int menfi(int[] arry)
+        {
+            int arry2;
+            for (int i = 0; i < arry.Length; i++)
+            {
+                if (arry[i] < 0)
+                {
+                    arry2 = arry[i] * (-1);
+                }
+                else (arry[i] > 0)
+                {
+                    arry2 = arry[i] * 1;
+                }
+                arry2 = arry1;
+            }
+            return arry2 ;
+           
+        }
+        
+    }
+}
